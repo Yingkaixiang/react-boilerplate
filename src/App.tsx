74a28de3889bbb4865f5da5e91a47af0996c9ac1
Yingkaixiang <rubykaikai@gmail.com>
2019-06-27 15:05:46 +0800

    feat: add layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,10 @@ import { Button } from "antd";
 import logo from "./logo.svg";
 import "./App.css";
 import styles from "./App.module.scss";
+import Layout from "./Layout";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button className="App-button">I love React</Button>
-        <p className={styles["App-text"]}>React NO.1</p>
-      </header>
-    </div>
-  );
+  return <Layout />;
 };
 
 export default App;
