3a00634cd1dfd23bae3a0ca530d1b229c0cacd5b
Yingkaixiang <rubykaikai@gmail.com>
2019-06-27 15:16:28 +0800

    feat: add i18n

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { LocaleProvider } from "antd";
+import moment from "moment";
+
+import zh_CN from "antd/lib/locale-provider/zh_CN";
+import "moment/locale/zh-cn";
+
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+moment.locale("zh-cn");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  (
+    <LocaleProvider locale={zh_CN}>
+      <App />
+    </LocaleProvider>
+  ),
+  document.getElementById("root"),
+);
+
+serviceWorker.register();
